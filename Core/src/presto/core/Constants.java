8e2d626c6f8eef9b8a74d6ada8594493fa4e7629
ericvergnaud <eric.vergnaud@wanadoo.fr>
2015-04-01 22:53:43 +0800

    application project wizard

--- Core/src/presto/core/Constants.java
+++ Core/src/presto/core/Constants.java
@@ -9,10 +9,13 @@ public abstract class Constants {
 
 	public static final String NEW_SCRIPTS_PROJECT = "New Presto Scripts Project";
 	public static final String NEW_FRAMEWORK_PROJECT = "New Presto Framework Project";
+	public static final String NEW_APPLICATION_PROJECT = "New Presto Application Project";
 	public static final String CREATING_PROJECT = "Creating project...";
 	public static final String CREATING_SAMPLE_SCRIPT = "Creating sample script...";
+	public static final String CREATING_SAMPLE_APPLICATION = "Creating sample application...";
 	public static final String NEW_PROJECT_ERROR = "Error while creating project";
 	public static final String VARIANT_PROJECT_EXISTS = "Another project with same name but different case already exists!";
 	public static final String SCRIPTS_PROJECT_DESCRIPTION = "A Presto project where each file is a standalone script";
 	public static final String FRAMEWORK_PROJECT_DESCRIPTION = "A Presto project for reusable attributes, categories and methods";
+	public static final String APPLICATION_PROJECT_DESCRIPTION = "A Presto project for a runnable aplication";
 }
